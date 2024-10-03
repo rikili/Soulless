79451878da247f86c577b77c4ea8a7678f88fcc5
Ricky <rickyzhl@students.cs.ubc.ca>
2024-10-03 10:56:16 -0700

    add combat components

--- project/include/entities/general_components.hpp
+++ project/include/entities/general_components.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "core/common.hpp"
+#include <core/common.hpp>
+#include <utils/constants.hpp> 
 
 // Motion Component
 struct Motion {
@@ -9,6 +10,7 @@ struct Motion {
     vec2 scale = { 1, 1 };
     float mass = 0;
     float speedModifier = 1.f;
+    unsigned int angle = 0;
 };
 
 // Resistance Modifier
@@ -23,6 +25,31 @@ struct Health
     ResistanceModifier resistance_modifier;
 };
 
+// Damage Component
+struct Damage
+{
+    float value = 0.f;
+    DamageType type;
+};
+
+// Type Components
+struct RangedEnemy { };
+struct MeleeEnemy { };
+struct Player {
+    DamageType right_hand;
+    DamageType left_hand;
+    float cooldown = -1.f;
+    unsigned int health_gauge = 0;
+    std::vector<DamageType> spell_queue;
+};
+struct Projectile {
+    DamageType type;
+    bool from_enemy;
+};
+
+// Timed Component
+struct Timed { }; // include temporary effects and counters
+
 // Structure to store collision information
 struct Collision
 {
@@ -30,7 +57,3 @@ struct Collision
     Entity other{}; // the second object involved in the collision
     explicit Collision(Entity& other) { this->other = other; };
 };
-
-
-
-
---
+++ project/include/utils/constants.hpp
@@ -0,0 +1,29 @@
+enum class DamageType
+{
+    fire,
+    ice,
+    lightning,
+    water,
+    plasma,
+    wind,
+    enemy,
+    elementless
+};
+
+// Spell/Projectile Constants
+
+// RANGED ENEMY
+float RANGED_BASIC_VELOCITY = 1.f;
+
+// FIRE
+float FIRE_VELOCITY = 1.f;
+
+// ICE
+float ICE_VELOCITY = 1.f;
+float ICE_DEGREE_DIFFERENCE = 15.f;
+int ICE_SHARD_COUNT = 4;
+
+// LIGHTNING
+// PLASMA
+// WATER
+// WIND
